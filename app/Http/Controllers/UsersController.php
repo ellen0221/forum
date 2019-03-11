51d776daa17c2cd0134370e9061c87f287d02997
vagrant <vagrant@homestead>
2019-03-11 08:03:16 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    // 注册
     public function create()
     {
         return view('users.create');
     }
+
+    // 用户页面
+    public function show(User $user)
+    {
+        // 将用户对象 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法，将数据与视图进行绑定。
+        // 能在视图中使用 user 变量来访问通过 view 方法传递给视图的用户数据。
+        return view('users.show', compact('user'));
+    }
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // 生成用户头像
+    public function gravatar($size = '100')
+    {
+        // Gravatar 为 “全球通用头像”，当你在 Gravatar 的服务器上放置了自己的头像后，
+        // 可通过将自己的 Gravatar 登录邮箱进行 MD5 转码，并与 Gravatar 的 URL 进行拼接来获取到自己的 Gravatar 头像。
+        // $this->attributes['email'] 获取到用户的邮箱
+        // trim 方法剔除邮箱的前后空白内容
+        // 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
