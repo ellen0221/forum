68706bfde34122dd07b9de6798a6500d0ae047a2
vagrant <vagrant@homestead>
2019-03-26 07:06:38 +0000

    移除邮件发送方法里的 form 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -80,13 +80,13 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'jat1014@163.com';
-        $name = 'xj';
+//        $from = 'jat1014@163.com';
+//        $name = 'xj';
         $to = $user->email;
         $subject = "感谢注册 WStory 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
